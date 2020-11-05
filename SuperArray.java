507db307fcd8cb42ccbdba19b11719aa44cbe1f5
lamdaham <ilam21@stuy.edu>
2020-11-05 09:51:25 -0500

    Debugged remove() from testcases

--- SuperArray.java
+++ SuperArray.java
@@ -101,7 +101,7 @@ public class SuperArray {
 
 	public String remove(int index) {
 		String output = data[index];
-		for (int i = index; i<size-2; i++) {
+		for (int i = index; i<size-1; i++) {
 			data[i] = data[i+1];
 		}
 		size--;
