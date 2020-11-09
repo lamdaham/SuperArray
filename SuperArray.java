82d31627b2fdab5ad0ddc676512ffd67507ce174
lamdaham <ilam21@stuy.edu>
2020-11-09 15:52:53 -0500

    Fixed add bounds

--- SuperArray.java
+++ SuperArray.java
@@ -100,7 +100,7 @@ public class SuperArray {
 	}
 
 	public void add(int index, String element) {
-		if (index<0||index>=size) {
+		if (index<0||index>size) {
 			throw new IndexOutOfBoundsException();
 		}
 		resize();
